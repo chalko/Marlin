becfb7891f46c837759e7101ce4dc5cac924c437
Nick Chalko <nchalko@google.com>
2017-12-09 12:40:10 -0800

    turn on bed PID

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -433,7 +433,7 @@
  *    M908 - BQ_ZUM_MEGA_3D, RAMBO, PRINTRBOARD_REVF, RIGIDBOARD_V2 & SCOOVO_X9H
  *    M909, M910 & LCD - only PRINTRBOARD_REVF & RIGIDBOARD_V2
  */
-#define PWM_MOTOR_CURRENT { 1020, 0390, 1020 }          // Values in milliamps  XY Z E
+#define PWM_MOTOR_CURRENT { 1020, 390, 1020 }          // Values in milliamps  XY Z E
 //#define DIGIPOT_MOTOR_CURRENT { 135,135,135,135,135 }   // Values 0-255 (RAMBO 135 = ~0.75A, 185 = ~1A)
 //#define DAC_MOTOR_CURRENT_DEFAULT { 70, 80, 90, 80 }    // Default drive percent - X, Y, Z, E axis
 
