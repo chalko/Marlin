f0270ea609b63bb7b224494cc340e755383be94e
Nick Chalko <nchalko@google.com>
2018-01-09 16:13:47 -0800

    Measured E steps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -539,7 +539,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 873 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 801 }
 
 /**
  * Default Max Feed Rate (mm/s)
